04fa4ec6260ad2ee8538c17fb1d5f391458c8b0a
rws205 <rws205@gmail.com>
2018-02-23 16:56:34 +0800

    update gs

--- GroundStation/mainwindow.cpp
+++ GroundStation/mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QTimer>
+#include <stdio.h>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -13,16 +13,17 @@ MainWindow::MainWindow(QWidget *parent) :
     page->load(QUrl(QStringLiteral("https://maps.google.com")));
 
     m_client = new QTcpSocket(this);
-    connect(m_client,SIGNAL(readyRead()),this,SLOT(slotReadyRead()));
-    connect(m_client,SIGNAL(connected()),this,SLOT(slotConnected()));
-    connect(m_client,SIGNAL(disconnected()),this,SLOT(slotDisConnected()));
+    connect(m_client,SIGNAL(readyRead()),this,SLOT(readyRead()));
+    connect(m_client,SIGNAL(connected()),this,SLOT(connected()));
+    connect(m_client,SIGNAL(disconnected()),this,SLOT(disConnected()));
 
-    QTimer *timer = new QTimer(this);
-    connect(timer, SIGNAL(timeout()), this, SLOT(getInfo()));
-    timer->start(1000);
+    isConnect = false;
+    tim_info = new QTimer(this);
+    connect(tim_info, SIGNAL(timeout()), this, SLOT(getInfo()));
 
     QString ip_address="123.3.2.1";
     m_client->connectToHost(ip_address, 80);
+    tim_info->start(1000);
 }
 
 MainWindow::~MainWindow()
@@ -30,13 +31,49 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::readyRead()
+{
+    printf("ready read\n");
+}
+
+void MainWindow::connected()
+{
+    printf("connect\n");
+}
+
+void MainWindow::disConnected()
+{
+    printf("disconnect\n");
+}
+
 void MainWindow::getInfo()
 {
+    static int cnt = 0;
     char cmd[50];
     cmd[0] = '@';
     cmd[1] = '#';
-    const char *str = "Hello Qt!";
-    memcpy(&cmd[2], str, strlen(str));
+//    const char *str = "Hello Qt!";
+//    memcpy(&cmd[2], str, strlen(str));
 
-    m_client->write(cmd, 2+strlen(str));
+    m_client->write(cmd, 2);//+0*strlen(str));
+    ui->pitch->setText(QString::number(cnt++));
+}
+
+void MainWindow::on_btn_connect_clicked()
+{
+    if (!isConnect)
+    {
+        tim_info->start(1000);
+        QString ip_address="123.3.2.1";
+        m_client->connectToHost(ip_address, 80);
+        ui->btn_connect->setText("Disconnect");
+        isConnect = true;
+    }
+    else {
+        isConnect = false;
+        tim_info->stop();
+        m_client->disconnected();
+        m_client->close();
+        ui->btn_connect->setText("Connect");
+    }
 }
--- GroundStation/mainwindow.h
+++ GroundStation/mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QWebEngineView>
 #include <QTcpSocket>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -21,10 +22,16 @@ private:
     Ui::MainWindow *ui;
     QWebEngineView *m_view;
     QTcpSocket *m_client;
+    QTimer *tim_info;
+
+    bool isConnect;
 
 private slots:
     void getInfo();
-
+    void readyRead();
+    void connected();
+    void disConnected();
+    void on_btn_connect_clicked();
 };
 
 #endif // MAINWINDOW_H
